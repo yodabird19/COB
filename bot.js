081e685dcfc6e0027074d87331a43ecfc47ddc57
Gjum <code.gjum@gmail.com>
2021-08-06 23:19:08 +0200

    heroku setup

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+const { DISCORD_BOT_TOKEN } = process.env;
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+    client.user.setPresence({
+        status: 'online',
+        activity: {
+            name: 'we do a little botting',
+            type: 'LISTENING',
+        },
+    });
+    console.log(client);
+});
+
+client.on('message', (message) => {
+    if (message.content == 'cob!helloworld') {
+        message.channel.send('hello world');
+    }
+});
+
+client.login(token);
